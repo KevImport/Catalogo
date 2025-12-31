457a3f2ca68b24d40a55bfbbd0c3f21f2327a5eb
KevImport <Kevimport25@gmail.com>
2025-12-30 23:33:27 -0500

    Update productos.js

--- productos.js
+++ productos.js
@@ -2512,10 +2512,10 @@ const productos = [
   categoria: "Navidad / Regalos",
   descripcion: "¡Pon a prueba tu agilidad mental con ¡Basta!, el emocionante juego donde debes pensar rápido y contestar antes que los demás! Esta versión electrónica incluye luces y un mazo de cartas para garantizar horas de diversión competitiva y risas con toda la familia.",
   precios: {
-   "1": 16.90,
+   "1": 14.95,
      },
   imagen: "Img/Imagen400.jpeg",
-  stock: 8
+  stock: 2
 },
     {
   nombre: "5436CAJA DE 50 MASCARILLAS DE TRES PLIEGUES",
@@ -2554,6 +2554,7 @@ const productos = [
   stock: 3
 }
 ];
+
 
 
 
